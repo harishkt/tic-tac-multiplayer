94a4a4abd072652e7916368cb0482cea938c5ab2
hkumar <harisht@udel.edu>
2018-06-05 14:58:45 -0400

    update server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (isDevelopment) {
 	app.use(webpackHotMiddleware(compiler));
 	// Route Method
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, './src/index.html'));
+		res.sendFile(path.join(__dirname, './dist/index.html'));
 	});
 	
 } else {
